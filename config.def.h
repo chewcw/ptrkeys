d950840e064e165476dd19d08d43af3af2a48527
ChewCW <chwai87@gmail.com>
2026-09-21 20:47:26 +0800

    config: bind Mod4Mask+XK_m to togglegrabkeyboard2

--- config.def.h
+++ config.def.h
@@ -41,11 +41,12 @@ static Key keys[] = {
 // If the keyboard will be grabbed while a key is held down, auto-repeat must
 // be disabled for the key using the NOREPEAT option. This could be
 // inconvenient if the key is frequently used outside of ptrkeys.
-{Mod4Mask,   XK_m,          GRAB,           togglegrabkeyboard2, {0},              NULL,            {0}},
-{0,          XK_q,          0,              ungrabkeyboard,      {0},              NULL,            {0}},
+// {Mod4Mask,   XK_m,          GRAB,           togglegrabkeyboard2, {0},              NULL,            {0}},
+// {0,          XK_q,          0,              ungrabkeyboard,      {0},              NULL,            {0}},
 // {0,          XK_Select,     GRAB|NOREPEAT,  grabkeyboard,        {0},              ungrabkeyboard,  {0}},
 // {ShiftMask,  XK_Select,     GRAB|NOREPEAT,  grabandmove2scroll,  {0},              NULL,            {0}},
 // {Mod4Mask,   XK_m,         GRAB,           togglegrabkeyboard,  {0},              NULL,            {0}},
+{Mod4Mask,   XK_m,         GRAB,           togglegrabkeyboard2,  {0},              NULL,            {0}},
 // {0,          XK_x,          0,              quit,                {0},              NULL,            {0}},
 // Directional control with vim hjkl.
 {0,          XK_h,          0,              movestart,           {.i=LEFT},        movestop,        {.i=LEFT}},
